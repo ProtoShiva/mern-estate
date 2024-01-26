a1c086238398f42df1ee477e31b3099246e3fd16
Himanshu <hgusain088@gmail.com>
2024-01-26 21:07:28 +0530

    data base connected

--- server/index.js
+++ server/index.js
@@ -1,4 +1,15 @@
 import express from "express"
+import mongoose from "mongoose"
+import "dotenv/config"
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB")
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
 const app = express()
 const port = 3000
