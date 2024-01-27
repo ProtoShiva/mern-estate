ed649cd0307c05a5530d953e820305a64dbf5160
Himanshu <hgusain088@gmail.com>
2024-01-27 17:01:12 +0530

    create signin api route

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 const Header = () => {
   return (
     <header className="bg-slate-200 shadow-md">
-      <div className="flex justify-between place-items-center">
+      <div className="flex justify-between place-items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap ">
             <span className="text-slate-500 ">Warner</span>
